8c4e1f03785500717a6f033753aa282e4a62208c
jv5547-dev <jv5547@srmist.edu.in>
2026-03-17 12:48:13 +0530

    added uc3- display hello with command line message

--- src/main/HelloApp.java
+++ src/main/HelloApp.java
@@ -1,11 +1,9 @@
 public class HelloApp {
     public static void main(String[] args) {
-        if (args.length > 0) {
-            String name = args[0];
-            System.out.println("Hello, " + name + " ! ");
-
-        }
-        else
-        System.out.println("Hello world " );
+       String name = "World";
+       if(args.length > 0){
+           name = args[0];
+       }
+       System.out.println("Hello, " + name + "!");
     }
 }
